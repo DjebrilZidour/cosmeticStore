b09785159d57530cc8d1e64b7e02fa657c1fb272
Djebril <djebrilacademy@gmail.com>
2024-04-21 18:35:43 +0100

    add react scroll

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,3 @@
-
 const Navbar = () => {
   return (
     <nav
@@ -6,20 +5,19 @@ const Navbar = () => {
       className=" px-16 py-2 w-full"
     >
       <ul className="list-none flex justify-between items-center">
-
         <li className="flex justify-center items-center gap-2">
-          <img href="https://ibb.co/FJMHH19" className="  h-14 w-14 bg-white rounded-full" src="https://i.ibb.co/Mp4BBHb/logo-Djazirat-El-Marra-1.png" />
-          
+          <img
+            href="https://ibb.co/FJMHH19"
+            className="  h-14 w-14 bg-white rounded-full"
+            src="https://i.ibb.co/Mp4BBHb/logo-Djazirat-El-Marra-1.png"
+          />
         </li>
 
         <div>
-        <h1 className="text-lg">Djazirat Elmarra</h1>
+          <h1 className="text-lg">Djazirat Elmarra</h1>
         </div>
 
-        <div className="flex justify-between items-center text-black-100 ">
-        
-        </div>
-        
+        <div className="flex justify-between items-center text-black-100 "></div>
       </ul>
     </nav>
   );
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,4 +1,5 @@
 import ProductCard from "./ProductCard";
+import { Link } from "react-scroll";
 
 const products = [
   {
