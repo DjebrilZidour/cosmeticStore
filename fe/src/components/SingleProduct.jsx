a7f0b4d6fa8885df1046d74a1fe9ea2e9050b6cb
Djebril <djebrilacademy@gmail.com>
2024-08-25 15:39:09 +0100

    setting up the system

---
+++ fe/src/components/SingleProduct.jsx
@@ -0,0 +1,18 @@
+const SingleProduct= (props)=>{
+return(
+    <section>
+        <div>
+            <h1>
+                
+            </h1>
+        </div>
+        <div>
+            <div><img src="" alt="" /></div>
+            <div>
+                <img src="" alt="" /><img src="" alt="" /><img src="" alt="" /><img src="" alt="" />
+            </div>
+        </div>
+    </section>
+)
+}
+export default SingleProduct
