e804331c327c81f886bf1bdb4f1ff538f2799bef
Djebril <djebrilacademy@gmail.com>
2024-09-01 23:08:10 +0200

    everything good for total

--- fe/src/pages/Admin.jsx
+++ fe/src/pages/Admin.jsx
@@ -3,13 +3,15 @@ import { useNavigate } from "react-router-dom";
 
 const Admin = () => {
   const navigate = useNavigate();
-  
+
   const LOCAL_STORAGE_KEYS = {
     isLogged: "isUserLoggedIn",
   };
 
   const getIsUserLogged = () => {
-    const isUserLogged = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEYS.isLogged));
+    const isUserLogged = JSON.parse(
+      localStorage.getItem(LOCAL_STORAGE_KEYS.isLogged)
+    );
     return typeof isUserLogged === "boolean" ? isUserLogged : false;
   };
 
@@ -23,13 +25,16 @@ const Admin = () => {
 
   useEffect(() => {
     const getCommands = async () => {
-      const response = await fetch("https://cosmeticstore-2ijr.onrender.com/product", {
-        method: "GET",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
-      });
+      const response = await fetch(
+        "https://cosmeticstore-2ijr.onrender.com/product",
+        {
+          method: "GET",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+        }
+      );
       const responseData = await response.json();
       setMyCommands(responseData.data);
     };
@@ -39,8 +44,10 @@ const Admin = () => {
 
   return (
     <div className="p-6 bg-gray-100 min-h-screen">
-      <h1 className="text-4xl font-bold text-center text-green-600 mb-8">Client Orders</h1>
-      
+      <h1 className="text-4xl font-bold text-center text-green-600 mb-8">
+        Client Orders
+      </h1>
+
       <div className="overflow-x-auto">
         <table className="min-w-full bg-white border border-gray-300">
           <thead>
@@ -55,22 +62,35 @@ const Admin = () => {
               <th className="py-3 px-2 text-left">Shipping Price</th>
               <th className="py-3 px-2 text-left">Order Time</th>
               <th className="py-3 px-2 text-left">total</th>
-              
+              <th>actions</th>
             </tr>
           </thead>
           <tbody className="text-gray-700 text-sm">
-            {myCommands.map((command, index) => (
-              <tr key={index} className="border-b border-gray-200 hover:bg-gray-100">
-                <td className="py-3 px-2 text-left whitespace-nowrap">{command.name}</td>
+            {myCommands.map((command, index) =>(
+              <tr
+                key={index}
+                className="border-b border-gray-200 hover:bg-gray-100"
+              >
+                <td className="py-3 px-2 text-left whitespace-nowrap">
+                  {command.name}
+                </td>
                 <td className="py-3 px-2 text-left">{command.phone}</td>
                 <td className="py-3 px-2 text-left">{command.product}</td>
                 <td className="py-3 px-2 text-left">{command.qte}</td>
                 <td className="py-3 px-2 text-left">{command.wilaya}</td>
                 <td className="py-3 px-2 text-left">{command.commune}</td>
                 <td className="py-3 px-2 text-left">{command.price} DA</td>
-                <td className="py-3 px-2 text-left">{command.shippingPrice} DA</td>
+                <td className="py-3 px-2 text-left">
+                  {command.shippingPrice} DA
+                </td>
                 <td className="py-3 px-2 text-left">{command.orderTime}</td>
-                <td className="py-3 px-2 text-left">{command.total} DA</td>
+                <td className="py-3 px-2 text-left">{command.price * command.qte + command.shippingPrice} DA</td>
+                <td className="flex justify-center items-center gap-4 bg-red-100">
+                  {" "}
+                  <button>approuve </button>
+                  <button>recall</button>
+                  <button>cancel</button>
+                </td>
               </tr>
             ))}
           </tbody>
